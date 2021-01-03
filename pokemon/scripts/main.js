51fa6b167844745c96611f1e6a26b77893c103f1
Snigdho Dip Howlader <61914319+snigdho611@users.noreply.github.com>
2021-01-03 22:29:42 +0600

    Update main.js

--- pokemon/scripts/main.js
+++ pokemon/scripts/main.js
@@ -2,17 +2,17 @@ function evolve1()
 {
     //console.log(document.getElementById('myImage').src);
     console.log(document.getElementById('myHint').textContent);
-    if(document.getElementById('myImage').src == 'http://localhost/JSpractice/images/pichu.png')
+    if(document.getElementById('myImage').src == 'https://raw.githubusercontent.com/snigdho611/snigdho611.github.io/main/pokemon/images/pichu.png')
     {
-        document.getElementById('myImage').src='http://localhost/JSpractice/images/pikachu.png';
+        document.getElementById('myImage').src='https://raw.githubusercontent.com/snigdho611/snigdho611.github.io/main/pokemon/images/pikachu.png';
     }
 
-    else if(document.getElementById('myImage').src == 'http://localhost/JSpractice/images/pikachu.png')
+    else if(document.getElementById('myImage').src == 'https://raw.githubusercontent.com/snigdho611/snigdho611.github.io/main/pokemon/images/pikachu.png')
     {
-        document.getElementById('myImage').src='http://localhost/JSpractice/images/raichu.png';
+        document.getElementById('myImage').src='https://raw.githubusercontent.com/snigdho611/snigdho611.github.io/main/pokemon/images/raichu.png';
     }
 
-    else if(document.getElementById('myImage').src == 'http://localhost/JSpractice/images/raichu.png')
+    else if(document.getElementById('myImage').src == 'https://raw.githubusercontent.com/snigdho611/snigdho611.github.io/main/pokemon/images/raichu.png')
     {
         document.getElementById('myHint').textContent='Raichu cannot evolve!';
     }
@@ -20,7 +20,7 @@ function evolve1()
 
 function reset()
 {
-    document.getElementById('myImage').src='http://localhost/JSpractice/images/pichu.png';
+    document.getElementById('myImage').src='https://raw.githubusercontent.com/snigdho611/snigdho611.github.io/main/pokemon/images/pichu.png';
     document.getElementById('myHint').textContent='';
 }
 
