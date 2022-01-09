4686ca5f2046d3b84a6186d51f8130d189ff9e0a
snigdho611 <snigdho.howlader@gmail.com>
2022-01-09 19:29:45 +0600

    Updated global css

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -2,49 +2,12 @@ import React from "react";
 
 const index = () => {
   return (
-    <div
-      style={{
-        marginTop: "120px",
-        marginLeft: "14%",
-        display: "flex",
-        flexDirection: "row",
-        width: "74%",
-      }}
-    >
-      <div
-        style={{
-          borderRadius: "100%",
-          backgroundColor: "#FFF",
-          width: "210px",
-          minWidth: "210px",
-          height: "210px",
-          backgroundImage: "url('/profilepic.jpg')",
-          backgroundSize: "cover",
-        }}
-      ></div>
-      <div
-        style={{
-          backgroundColor: "#5BC0DE",
-          height: "470px",
-          width: "25px",
-          margin: "0 90px",
-        }}
-      ></div>
+    <div className="About-container">
+      <div className="Profile-img" />
+      <div className="Bar" />
       <div>
-        <div
-          style={{
-            color: "#5BC0DE",
-            fontWeight: "bold",
-            fontSize: "32px",
-            textAlign: "center",
-            marginBottom: "10px",
-          }}
-        >
-          ABOUT
-        </div>
-        <div
-          style={{ color: "#BEEBF9", fontSize: "22px", textAlign: "justify" }}
-        >
+        <div className="Header">ABOUT</div>
+        <div className="About-text">
           An efficient Professional Software Developer with experience in the
           industry. Avid in both frontend and backend development technologies,
           aspiring for contribution to the world of computer science and
