67520182ad2510e16be26cb2a781e9a0134964c2
Snigdho Dip Howlader <snigdho.howlader@gmail.com>
2022-04-01 03:57:09 +0600

    Updated with gsap animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
+import gsap from "gsap";
 import type { NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
+import React, { useEffect, useRef } from "react";
 // import About from "components/About";
 // import Skills from "components/Skills";
 // import Experience from "components/Experience";
@@ -12,7 +15,29 @@ import Head from "next/head";
 // import { scroller } from "react-scroll";
 // import { useEffect, useState } from "react";
 
-const Home: NextPage = () => {
+interface HomeProps {
+  val: string;
+}
+
+const Home: React.FC<HomeProps> = ({ val }) => {
+  const textRef = useRef<HTMLDivElement>(null);
+
+  const onLoad = (currentTarget: React.RefObject<HTMLDivElement>) => {
+    gsap.fromTo(
+      currentTarget.current,
+      { scale: 15, delay: 0.7, x: 800, y: 350 },
+      { scale: 1, x: 0, y: 0, delay: 1, duration: 1 }
+    );
+  };
+
+  // const onUnload = ({ currentTarget }: any) => {
+  //   gsap.to(currentTarget, { scale: 1.0 });
+  // };
+
+  useEffect(() => {
+    onLoad(textRef);
+  }, []);
+
   return (
     <>
       <Head>
@@ -31,6 +56,29 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/images/favicon.png" />
       </Head>
+      {/* <div>{val}</div> */}
+      <div
+        style={{
+          // backgroundColor: "gray",
+          // backgroundImage: ".public/images/favicon.png",
+          minWidth: "100px",
+          width: "100px",
+          minHeight: "50px",
+          height: "50px",
+          // margin: "20px auto",
+        }}
+        ref={textRef}
+        // onMouseEnter={onLoad}
+        // onMouseLeave={onUnload}
+      >
+        {/* <img alt="empty" src="../" /> */}
+        <Image
+          src={"/images/favicon2.svg"}
+          width={100}
+          height={50}
+          alt="Image not found"
+        />
+      </div>
       {/* <div className="Navbar">
         <div className="Name">{allData.name}</div>
         <div className="links-container">
@@ -80,5 +128,14 @@ const Home: NextPage = () => {
     </>
   );
 };
+
+export async function getServerSideProps() {
+  // Fetch data from external API
+  // const res = await fetch(`https://.../data`)
+  // const data = await res.json()
+  const value = process.env.BASE_VAL;
+  // Pass data to the page via props
+  return { props: { val: value } };
+}
 
 export default Home;
