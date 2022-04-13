18b8ea0d0267f5ad16bed5f932d6235d856c61f1
Snigdho Dip Howlader <snigdho.howlader@gmail.com>
2022-04-13 06:04:26 +0600

    Added general animations for skillbox

--- src/components/Base/index.tsx
+++ src/components/Base/index.tsx
@@ -5,21 +5,21 @@ const Base = () => {
   const baseRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    gsap.fromTo(
-      baseRef.current,
-      { x: "60%", opacity: 0 },
-      { x: 0, y: 0, opacity: 1, duration: 1.75, delay: 3 }
-    );
+    // gsap.fromTo(
+    //   baseRef.current,
+    //   { x: "60%", opacity: 0 },
+    //   { x: 0, y: 0, opacity: 1, duration: 1.75, delay: 3 }
+    // );
   }, []);
 
   return (
     <div className="text-cyan-200 my-auto mx-auto my-60 w-3/4" ref={baseRef}>
-      <div className="text-6xl font-medium text-center my-5">
+      <h1 className="text-6xl font-medium text-center my-5">
         Snigdho Dip Howlader
-      </div>
-      <div className="text-4xl text-cyan-500 italic text-center my-5">
+      </h1>
+      <h1 className="text-4xl text-cyan-500 italic text-center my-5">
         Fullstack Software Engineer
-      </div>
+      </h1>
       <div className="text-2xl flex flex-row gap-x-14 mx-auto text-cyan-500 justify-center my-8">
         <div className="cursor-pointer text-cyan-500 hover:text-cyan-100 duration-300 transition">
           Frontend
--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,24 +1,71 @@
-import React from "react";
+import gsap from "gsap";
+import React, { useEffect, useRef } from "react";
+// import reactlogo from "../../../public/images/icons/react.svg";
 
 const Skills = () => {
+  const reactRef = useRef<HTMLDivElement>(null);
+  const laravelRef = useRef<HTMLDivElement>(null);
+
+  const rotateObjects = (objects: React.RefObject<HTMLDivElement>[]) => {
+    objects.map((object) => {
+      gsap.to(object.current, {
+        transformPerspective: 800,
+        transformOrigin: "center",
+        duration: 1,
+        rotationY: 360,
+      });
+    });
+  };
+
+  useEffect(() => {
+    const allRefs = [];
+    allRefs.push(reactRef, laravelRef);
+    rotateObjects(allRefs);
+  }, []);
+
   return (
-    <div
-      className="w-2/4 my-48 mx-auto"
-      style={{
-        // justifyContent: "center",
-        // alignContent: "center",
-        // justifyItems: "center",
-        // justifySelf: "center",
-        // justifyContent: "center",
-        alignContent: "center",
-        alignItems: "center",
-        alignSelf: "center",
-      }}
-    >
-      <div>
-        <h1>Frontend</h1>
+    <div className="w-2/4 my-48 mx-auto">
+      <div className="flex flex-col text-cyan-300">
         <div>
-          <div>React JS</div>
+          <h1 className="text-3xl">Frontend</h1>
+          <div className="flex flex-row">
+            <div
+              className="min-w-24 w-24 min-h-24 h-24 mx-5"
+              style={{
+                backgroundImage: `url('images/icons/reactjs.png')`,
+                backgroundSize: "100%",
+                backgroundRepeat: "no-repeat",
+              }}
+              ref={reactRef}
+            >
+              {/* React JS */}
+            </div>
+            <div className="w-24 h-24 mx-5">Vue JS</div>
+          </div>
+        </div>
+        <div>
+          <h1 className="text-3xl">Backend</h1>
+          <div className="flex flex-row">
+            <div
+              className="min-w-24 w-24 min-h-24 h-24 mx-5"
+              style={{
+                backgroundImage: `url('images/icons/laravel.png')`,
+                backgroundSize: "100%",
+                backgroundRepeat: "no-repeat",
+              }}
+              ref={laravelRef}
+            />
+            <div className="w-24 h-24 mx-5">Node JS</div>
+            <div className="w-24 h-24 mx-5">ASP.NET</div>
+          </div>
+        </div>
+        <div>
+          <h1 className="text-3xl">Database</h1>
+          <div className="flex flex-row">
+            <div className="w-24 h-24 mx-5">Oracle</div>
+            <div className="w-24 h-24 mx-5">PostgreSQL</div>
+            <div className="w-24 h-24 mx-5">MongoDB</div>
+          </div>
         </div>
       </div>
       {/* <div className="flex flex-row justify-center">
