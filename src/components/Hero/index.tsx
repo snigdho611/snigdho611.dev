fb0a9fa9c8bf27866f29d54ac58213a4d805219b
Snigdho Dip Howlader <snigdho.howlader@gmail.com>
2022-10-09 19:27:48 +0600

    Updated design of Hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -4,11 +4,6 @@ import { TypeAnimation } from "react-type-animation";
 
 const Hero = () => {
   const baseRef = useRef<HTMLDivElement>(null);
-  const [hoverIndex, setHoverIndex] = useState<Number>(-1);
-
-  useEffect(() => {
-    console.log("ok");
-  }, [hoverIndex]);
 
   const motionVariant = {
     visible: { opacity: 0.8, duration: 1, transform: "rotateX(360deg)" },
@@ -16,52 +11,48 @@ const Hero = () => {
     hover: { type: "spring", width: "80%", color: "white", opacity: 1 },
   };
 
-  const line1 = "Snigdho Dip Howlader";
-  const line2 = "Fullstack Software Engineer";
-
   return (
-    <motion.div
-      className="text-cyan-200 mx-auto my-60 w-3/4"
-      ref={baseRef}
-      // initial={{ scale: 0, opacity: 0 }}
-      // animate={{ scale: 1, opacity: 1 }}
-      // transition={{ duration: 1 }}
-    >
-      <h1 className="text-6xl font-medium text-center my-5">Snigdho Dip Howlader</h1>
-      <h1 className="text-cyan-500 text-center my-5">
-        <TypeAnimation
-          sequence={[
-            "Software Engineer",
-            2000,
-            "Software Engineer - Backend",
-            1500,
-            "Software Engineer - Frontend",
-            1500,
-            "Software Engineer - Testing",
-            1500,
-            "Software Engineer",
-            () => {
-              console.log("Done typing!");
-            },
-          ]}
-          repeat={0}
-          speed={5}
-          wrapper="div"
-          cursor={true}
-          // repeat={Infinity}
-          style={{ fontSize: "2em" }}
-        />
-      </h1>
-      <div className="text-2xl flex flex-row gap-x-5 mx-auto h-96 text-cyan-500 justify-center my-8 w-5/6">
+    <div className="my-40">
+      <div className="text-cyan-200 mx-auto w-3/4" ref={baseRef}>
+        <div className="flex flex-col px-28">
+          <label className="text-5xl font-medium">Snigdho Dip Howlader</label>
+          <label className="text-cyan-500 text-[15px] my-5">
+            <TypeAnimation
+              sequence={[
+                "Software Engineer",
+                3000,
+                "Software Engineer - Backend",
+                1500,
+                "Software Engineer - Frontend",
+                1500,
+                "Software Engineer - Testing",
+                1500,
+                "Software Engineer",
+                5000,
+              ]}
+              // repeat={0}
+              speed={5}
+              wrapper="div"
+              cursor={true}
+              repeat={Infinity}
+              style={{ fontSize: "2em" }}
+            />
+          </label>
+          {/* <label className="text-cyan-300 my-3">
+            A <strong>Professional Software Developer</strong> with experience in the industry. Avid
+            in both <strong>Frontend Development</strong> and <strong>Backend Development</strong>{" "}
+            technologies. Additionally, academically learned with Machine Learning, Data Mining and
+            Algorithms that contribute to problem solving skills.
+          </label> */}
+        </div>
+      </div>
+      <div className="text-2xl flex flex-row gap-x-5 mx-auto h-80 text-cyan-500 justify-center w-5/6">
         <motion.div
           variants={motionVariant}
           initial="start"
           animate="visible"
           whileHover="hover"
           transition={{ duration: 1, transform: { duration: 1, delay: 1 } }}
-          // onMouseEnter={() => {
-          //   setHoverIndex(0);
-          // }}
           className="cursor-pointer min-w-[200px] flex justify-center items-center bg-frontend bg-cover bg-center bg-no-repeat"
         >
           Frontend
@@ -72,9 +63,6 @@ const Hero = () => {
           initial="start"
           animate="visible"
           transition={{ duration: 1, transform: { duration: 1, delay: 1 } }}
-          // onMouseEnter={() => {
-          //   setHoverIndex(2);
-          // }}
           className="cursor-pointer min-w-[200px] flex justify-center items-center bg-backend bg-cover bg-center bg-no-repeat"
         >
           Backend
@@ -85,15 +73,12 @@ const Hero = () => {
           initial="start"
           animate="visible"
           transition={{ duration: 1, transform: { duration: 1, delay: 1 } }}
-          // onMouseEnter={() => {
-          //   setHoverIndex(1);
-          // }}
           className="cursor-pointer min-w-[200px] flex justify-center items-center bg-database bg-cover bg-center bg-no-repeat"
         >
           Databases
         </motion.div>
       </div>
-    </motion.div>
+    </div>
   );
 };
 
